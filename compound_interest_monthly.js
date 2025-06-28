e67ec853120d45a51b2450e9cc89a4c0ad67e39a
Nikolay Botev <nikolaybotev.b@gmail.com>
2025-06-28 16:18:30 -0700

    Fix one-off error and simplify.

--- compound_interest_monthly.js
+++ compound_interest_monthly.js
@@ -1,9 +1,9 @@
 // Brute force compound interest loan payment calculator
 
 // Inputs
-let amount = 855_000 // dollars
-let interest = 6.99 // percent per year
-let term = 30 // years
+const loan_amount = 855_000 // dollars
+const interest = 6.99 // percent per year
+const term = 30 // years
 
 // Utilities
 function amt(num) {
@@ -14,7 +14,7 @@ function amt(num) {
 let monthly_interest = (interest / 12) / 100
 let no_of_payments = term * 12
 // See https://www.bankrate.com/mortgages/amortization-calculator/#how-to-calculate
-let monthly_payment = Math.ceil(amount * (
+let monthly_payment = (loan_amount * (
     (monthly_interest * (1 + monthly_interest) ** no_of_payments) /
     ((1 + monthly_interest) ** no_of_payments - 1)
 ))
@@ -23,35 +23,27 @@ console.log("Computed monthly payment ", amt(monthly_payment))
 // Calculate  Payments
 let interest_paid = 0;
 let principal_paid = 0;
-let principal_left = amount;
-let interest_acc = 0;
+let principal_left = loan_amount;
 for (let year = 0; year < term; year += 1) {
     console.log("YEAR ", year, " START");
 
     for (let month = 0; month < 12; month += 1) {
-        if (interest_acc > 0) {
-            // payment
-            let payment_interest = interest_acc;
-            let payment_principal = monthly_payment - payment_interest;
-            console.log("  PAYMENT on YEAR ", year, " MONTH ", month, " interest " + amt(payment_interest) + " principal " + amt(payment_principal));
+        // payment
+        let payment_interest = monthly_interest * principal_left;
+        let payment_principal = monthly_payment - payment_interest;
+        console.log(
+            `  PAYMENT on YEAR  ${year}  MONTH  ${month}  interest ${amt(payment_interest)} principal ${amt(payment_principal)}`);
 
-            // update statistics
-            interest_paid += payment_interest;
-            principal_paid += payment_principal;
-            principal_left -= payment_principal;
-
-            // reset interest compounding
-            interest_acc = 0;
-        }
+        // update statistics
+        interest_paid += payment_interest;
+        principal_paid += payment_principal;
+        principal_left -= payment_principal;
 
         // Extra payment on the first month
         // if (year == 0 && month == 1) {
         //     principal_paid += 45;
         //     principal_left -= 45;
         // }
-
-        // Interet compounds monthly
-        interest_acc += monthly_interest * (principal_left + interest_acc);
     }
 
     console.log("YEAR ", year, " SUMMARY");
